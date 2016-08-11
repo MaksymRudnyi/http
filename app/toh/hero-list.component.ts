1629b52c445519406563969b7681415b575c820a
maxim <maxim@webro.com.ua>
2016-08-11 23:49:04 +0300

    remove Gooogle license

--- app/toh/hero-list.component.ts
+++ app/toh/hero-list.component.ts
@@ -32,10 +32,3 @@ export class HeroListComponent implements OnInit {
                 error =>  this.errorMessage = <any>error);
     }
 }
-
-
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
